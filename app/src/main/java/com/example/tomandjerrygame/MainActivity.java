e1b16706024da241e68f486b9f42252675f65790
Amos <amoshmuel1995@gmail.com>
2021-11-09 14:45:15 +0200

    vibrate

--- app/src/main/java/com/example/tomandjerrygame/MainActivity.java
+++ app/src/main/java/com/example/tomandjerrygame/MainActivity.java
@@ -1,37 +1,33 @@
 package com.example.tomandjerrygame;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.util.Random;
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ImageView[][] path;
+    private ImageView[][] panel_img_obstacle;
     private int[][] vals;
     private ImageView [] player;
     private ImageView [] live;
     private ImageButton btn_left;
     private ImageButton btn_right;
-    private Timer timer = new Timer();
-    private int clock = 1;
     private int  playerPosition =1;
-    int flag = 1; //1 = first open
-    final int DELAY = 2000;
+    final int DELAY = 1000;
     final Handler handler = new Handler();
     private Runnable timerRunnable;
-
-
-
+    private final int VIBRATE_TIME = 500;
+    int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,22 +35,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         findViews();
-/*
-        ImageView demo_IMG_back = findViewById(R.id.IMG_back);
-        Glide
-                .with(this)
-                .load(R.drawable.img_house_back)
-                .centerCrop()
-                .into(demo_IMG_back);
-*/
         for (int i = 0; i < vals.length; i++) {
             for (int j = 0; j < vals[i].length; j++) {
                 vals[i][j] = 0;
             }
         }
-
         initViews();
-//check it
+
         timerRunnable = () -> {
             updateClockView();
             handler.postDelayed(timerRunnable, DELAY);
@@ -71,13 +58,17 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 if (playerPosition == 0) {//player on the left move to mid
                     player[0].setVisibility(View.INVISIBLE);
+                    player[1].setImageResource(R.drawable.img_jerry);
                     player[1].setVisibility(View.VISIBLE);
                     playerPosition = 1;
                 } else if (playerPosition == 1) {//player on the mid move to right
                     player[1].setVisibility(View.INVISIBLE);
+                    player[2].setImageResource(R.drawable.img_jerry);
                     player[2].setVisibility(View.VISIBLE);
                     playerPosition = 2;
                 } else if (playerPosition == 2) { //player on the right cant move
+                    player[2].setImageResource(R.drawable.img_jerry);
+                    player[2].setVisibility(View.VISIBLE);
                     playerPosition = 2;
                 }
             }
@@ -88,13 +79,17 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 if (playerPosition == 2) {//player on the right move to mid
                     player[2].setVisibility(View.INVISIBLE);
+                    player[1].setImageResource(R.drawable.img_jerry);
                     player[1].setVisibility(View.VISIBLE);
                     playerPosition = 1;
                 } else if (playerPosition == 1) {//player on the mid move to left
                     player[1].setVisibility(View.INVISIBLE);
+                    player[0].setImageResource(R.drawable.img_jerry);
                     player[0].setVisibility(View.VISIBLE);
                     playerPosition = 0;
                 } else if (playerPosition == 0) { //player on the right cant move
+                    player[0].setImageResource(R.drawable.img_jerry);
+                    player[0].setVisibility(View.VISIBLE);
                     playerPosition = 0;
                 }
             }
@@ -102,64 +97,82 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void findViews() {
-        path = new ImageView[][]{
-                {findViewById(R.id.demo_IMG_00), findViewById(R.id.demo_IMG_01), findViewById(R.id.demo_IMG_02)},
-                {findViewById(R.id.demo_IMG_10), findViewById(R.id.demo_IMG_11), findViewById(R.id.demo_IMG_12)},
-                {findViewById(R.id.demo_IMG_20), findViewById(R.id.demo_IMG_21), findViewById(R.id.demo_IMG_22)},
-                {findViewById(R.id.demo_IMG_30), findViewById(R.id.demo_IMG_31), findViewById(R.id.demo_IMG_32)}
+        panel_img_obstacle = new ImageView[][]{
+                {findViewById(R.id.panel_img_tom_00), findViewById(R.id.panel_img_tom_01), findViewById(R.id.panel_img_tom_02)},
+                {findViewById(R.id.panel_img_tom_10), findViewById(R.id.panel_img_tom_11), findViewById(R.id.panel_img_tom_12)},
+                {findViewById(R.id.panel_img_tom_20), findViewById(R.id.panel_img_tom_21), findViewById(R.id.panel_img_tom_22)},
+                {findViewById(R.id.panel_img_tom_30), findViewById(R.id.panel_img_tom_31), findViewById(R.id.panel_img_tom_32)},
+                {findViewById(R.id.panel_img_jerry0), findViewById(R.id.panel_img_jerry1), findViewById(R.id.panel_img_jerry2)}
         };
-        vals = new int[path.length][path[0].length];
+        vals = new int[panel_img_obstacle.length][panel_img_obstacle[0].length];
         player = new ImageView[]{
-                findViewById(R.id.panel_IMG_jerry0),
-                findViewById(R.id.panel_IMG_jerry1),
-                findViewById(R.id.panel_IMG_jerry2)
+                findViewById(R.id.panel_img_jerry0),
+                findViewById(R.id.panel_img_jerry1),
+                findViewById(R.id.panel_img_jerry2)
         };
         live = new ImageView[]{
-                findViewById(R.id.panel_IMG_chesse1),
-                findViewById(R.id.panel_IMG_chesse2),
-                findViewById(R.id.panel_IMG_chesse3)
+                findViewById(R.id.panel_img_chesse1),
+                findViewById(R.id.panel_img_chesse2),
+                findViewById(R.id.panel_img_chesse3)
         };
-        btn_left = findViewById(R.id.btn_left_panel);
-        btn_right = findViewById(R.id.btn_right_panel);
+        btn_left = findViewById(R.id.panel_btn_left);
+        btn_right = findViewById(R.id.panel_btn_right);
 
     }
 
 
     private void checkCrash() {
-
         for (int i = 0; i < vals[vals.length - 1].length; i++) {
             if(vals[vals.length- 1][i] == 1 && playerPosition == i){
                 for (int j = live.length - 1; j >= 0; j--) {
                     if(live[j].getVisibility() == View.VISIBLE){
                         live[j].setVisibility(View.INVISIBLE);
+                        Toast.makeText(MainActivity.this, "HIT!", Toast.LENGTH_SHORT).show();
+                        vibrate();
                         return;
                     }else if (live[0].getVisibility() == View.INVISIBLE){
-                        onStop();
+                        stopTicker();
                     }
                 }
             }
         }
     }
 
+    private void vibrate() {
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            v.vibrate(VibrationEffect.createOneShot(VIBRATE_TIME, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+            //deprecated in API 26
+            v.vibrate(VIBRATE_TIME);
+        }
+    }
+
+
     private void runlogic() {
-        for (int j = 0; j < vals[0].length; j++) {
-            if(j == 0) {
-                vals[0][j] = 1;
-            } else {
+        if(counter++ %2 == 0){
+            for (int j = 0; j < vals[0].length; j++) {
+                if(j == 0) {
+                    vals[0][j] = 1;
+                } else {
+                    vals[0][j] = 0;
+                }
+            }
+            shuffleArray(vals);
+        }else {
+            for (int j = 0; j < vals[0].length; j++) {
                 vals[0][j] = 0;
             }
         }
-        shuffleArray(vals);
     }
 
     private void shuffleArray(int[][] vals) {
-        Random rnd = ThreadLocalRandom.current();
-        for (int i = vals[0].length - 1; i > 0; i--){
-            int index = rnd.nextInt(i + 1);
-            // Swap
-            int a = vals[0][index];
-            vals[0][index] = vals[0][i];
-            vals[0][i] = a;
+        Random rand = new Random();
+        for (int i = 0; i < vals[0].length; i++) {
+            int randomIndexToSwap = rand.nextInt(vals[0].length);
+            int temp = vals[0][randomIndexToSwap];
+            vals[0][randomIndexToSwap] = vals[0][i];
+            vals[0][i] = temp;
         }
     }
 
@@ -180,21 +193,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         startTicker();
-//        updateUI();
     }
 
     private void updateUI() {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 0; i < path.length; i++) {
-                    for (int j = 0; j < path[i].length; j++) {
-                        ImageView im = path[i][j];
+                for (int i = 0; i < panel_img_obstacle.length; i++) {
+                    for (int j = 0; j < panel_img_obstacle[i].length; j++) {
+                        ImageView im = panel_img_obstacle[i][j];
                         if(vals[i][j] == 0){
-                            path[i][j].setVisibility(View.INVISIBLE);
+                            panel_img_obstacle[i][j].setVisibility(View.INVISIBLE);
+                            player[playerPosition].setImageResource(R.drawable.img_jerry);
+                            player[playerPosition].setVisibility(View.VISIBLE);
                         }else if (vals[i][j] == 1){
-                            path[i][j].setVisibility(View.VISIBLE);
-                            path[i][j].setImageResource(R.drawable.img_tom);
+                            panel_img_obstacle[i][j].setVisibility(View.VISIBLE);
+                            panel_img_obstacle[i][j].setImageResource(R.drawable.img_tom);
+                            player[playerPosition].setImageResource(R.drawable.img_jerry);
+                            player[playerPosition].setVisibility(View.VISIBLE);
                         }
                     }
                 }
